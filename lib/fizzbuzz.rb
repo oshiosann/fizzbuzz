fe9bebca582869afa39332d1912dd2134bc84bd5
oshiosan <git.fq8qzpec@gmail.com>
2025-04-19 22:22:13 +0900

    Fix indent

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -6,7 +6,7 @@ class Fizzbuzz
       'fizz'
     elsif number % 5 == 0
       'buzz'
-       elasif number.to_s.include? '7'
+    elasif number.to_s.include? '7'
        'GitHub'
     else
       number
